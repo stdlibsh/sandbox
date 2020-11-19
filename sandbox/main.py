bc791f35c11cd3098dd7b5162c1c35b63298844c
Vlad Dmitrievich <me@2tunnels.com>
2020-11-19 11:37:45 +0100

    Build and push docker image on merge to master (#4)

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
+from sandbox import __version__
 from sandbox.routes import router
 
 
 def get_application() -> FastAPI:
-    app = FastAPI(title="Sandbox")
+    app = FastAPI(title="Sandbox", version=__version__)
     app.include_router(router)
 
     return app
